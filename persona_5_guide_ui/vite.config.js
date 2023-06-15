00353bc9c0426d3f21ae6099a6ee28f7a25afdfe
iker <iker@hey.com>
2023-06-15 14:48:02 +0200

    Add bae path.

--- persona_5_guide_ui/vite.config.js
+++ persona_5_guide_ui/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/persona5_guide/" : "/",
   plugins: [vue({
     compilerOptions : {
       whitespace: 'preserve'
